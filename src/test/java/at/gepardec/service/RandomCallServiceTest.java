f9e776eb55a896d35581e0ec9e5936ce57c8a3ab
Alexander Schober <alex.schober1@gmx.at>
2022-05-23 18:13:41 +0200

    Renamed RandomCallServiceTest to getServiceForUnknownHostThrowsProcessingException

--- src/test/java/at/gepardec/service/RandomCallServiceTest.java
+++ src/test/java/at/gepardec/service/RandomCallServiceTest.java
@@ -1,17 +1,10 @@
 package at.gepardec.service;
 
-import com.google.common.base.Supplier;
-import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
 import javax.ws.rs.ProcessingException;
-import javax.ws.rs.core.Response;
 
-import java.net.UnknownHostException;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,7 +16,7 @@ class RandomCallServiceTest {
 
 
     @Test
-    void getRandomServiceTest() {
+    void getServiceForUnknownHostThrowsProcessingException() {
 
         RandomCallService randomCallService = new RandomCallService(List.of("http://www.bielefeld.test"), 1858L);
         //then
