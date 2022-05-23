b68a304dda29141d0c16ef8f6a4039f3f186693f
Mario Oberwalder <mario.oberwalder@gepardec.com>
2022-05-23 15:21:27 +0200

    GPX-262 test for endpoint /logging/error removed unused annotations removed unnused imports

--- src/test/java/at/gepardec/rest/LogErrorGeneratorResourceTest.java
+++ src/test/java/at/gepardec/rest/LogErrorGeneratorResourceTest.java
@@ -1,32 +1,21 @@
 package at.gepardec.rest;
 
-import io.quarkus.test.common.http.TestHTTPEndpoint;
-import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import javax.ws.rs.core.Response;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class LogErrorGeneratorResourceTest {
 
-    @TestHTTPEndpoint(LogErrorGeneratorResource.class)
-    @TestHTTPResource
-    URL url;
-
     @Test
-    void logErrorMessageTest() throws IOException {
-        try (InputStream in = url.openStream()) {
-            String contents = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-            Assertions.assertEquals("hello", contents);
-        }
+    void getCorrectResponseFromErrorEndpoint() {
+        given()
+                .when().get("/logging/error")
+                .then()
+                .statusCode(200)
+                .body(is("Logged error message"));
     }
 
     @Test
