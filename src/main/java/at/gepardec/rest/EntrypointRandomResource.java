d6c692b9440d564fad24d630441f55203b5703cc
Alexander Schober <alex.schober1@gmx.at>
2022-05-30 19:34:01 +0200

    GPX-280 Implemented user supplied sequence CallService

--- src/main/java/at/gepardec/rest/EntrypointRandomResource.java
+++ src/main/java/at/gepardec/rest/EntrypointRandomResource.java
@@ -1,6 +1,6 @@
 package at.gepardec.rest;
 
-import at.gepardec.service.OrderedCallService;
+import at.gepardec.service.RandomCallService;
 import at.gepardec.service.ServiceCollector;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.metrics.MetricUnits;
@@ -8,6 +8,7 @@ import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 import org.jboss.logging.Logger;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -15,11 +16,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.util.Random;
 import java.util.UUID;
 
 @Path("/call")
 @ApplicationScoped
-public class EntrypointResource {
+public class EntrypointRandomResource {
 
     @Inject
     Logger Log;
@@ -27,46 +29,39 @@ public class EntrypointResource {
     @ConfigProperty(name = "microservices.idletime")
     int idletime;
 
-    @ConfigProperty(name = "microservices.sequence")
-    String orderSequence;
+    @ConfigProperty(name = "microservices.seed")
+    Long seed;
+
+    Random random;
 
     @Inject
     ServiceCollector serviceCollector;
 
+    @PostConstruct
+    void initRandom() {
+        random = new Random(seed);
+    }
+
     @GET
-    @Path("/serviceBySequence")
+    @Path("/service")
     @Counted(name = "performedCalls", description = "How often the service has been called.")
     @Timed(name = "callsTimer", description = "A measure of how long it takes to perform the complete call.", unit = MetricUnits.MILLISECONDS)
     @Produces(MediaType.TEXT_PLAIN)
-    public void callNextService(@QueryParam("orderSequence") String orderSequence,
-                                @QueryParam("transactionID") UUID transactionID) {
-        this.orderSequence = orderSequence;
+    public void callNextService(@QueryParam("ttl") int ttl,
+                                @QueryParam("transactionID") UUID transactionID)
+            throws InterruptedException {
+
+        Thread.sleep(idletime);
         Log.info("Service 1 requesting call of next Service");
-        if(orderSequence.length() == 0) {
-            return;
-        }
-        char currentchar = parseOrderSequence();
-        chooseActionByChar(currentchar, transactionID);
-
+        callRandomService(ttl, transactionID);
     }
 
-    private void chooseActionByChar(char currentchar, UUID transactionID) {
-        callService(orderSequence, transactionID);
-    }
-
-    private char parseOrderSequence() {
-
-        char currentChar = orderSequence.charAt(0);
-        orderSequence = orderSequence.substring(1);
-        return currentChar;
-
-    }
-
-    public void callService(String orderSequence, UUID transactionID) {
-        if (orderSequence.length() > 0) {
+    public void callRandomService(int ttl, UUID transactionID) {
+        if (ttl > 0) {
             Log.info("TransactionID: " + transactionID.toString() + " - Calling Random service");
-            OrderedCallService orderedCallService = new OrderedCallService(serviceCollector.getServiceURLs());
-            orderedCallService.callRandomServiceBySequence(orderSequence, transactionID);
+            Log.info("ttl: " + (ttl - 1));
+            RandomCallService randomCallService = new RandomCallService(serviceCollector.getServiceURLs(), random);
+            randomCallService.callRandomService(--ttl, transactionID);
         } else {
             Log.info("[" + transactionID.toString() + "]" + " Stopping RandomCallService...");
         }
