d2b79b775a2a22c58b794d2fe8ae10d99fd333ea
Mario Oberwalder <mario.oberwalder@gepardec.com>
2022-06-01 10:28:52 +0200

    GPX-280 added Action enum

---
+++ src/main/java/at/gepardec/service/ServiceAction.java
@@ -0,0 +1,14 @@
+package at.gepardec.service;
+
+public enum ServiceAction {
+    OK1,
+    ERROR1,
+    ONEOUTTENERROR1,
+    ONEOUTHUNDREDERROR1,
+    DELAY1,
+    OK2,
+    ERROR2,
+    ONEOUTTENERROR2,
+    ONEOUTHUNDREDERROR2,
+    DELAY2
+}
