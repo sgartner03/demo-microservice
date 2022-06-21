7f206a75f169d6e5fe9649f8084eee24f642ea1f
Alexander Schober <alex.schober1@gmx.at>
2022-06-21 12:02:34 +0200

    GPX-262 added cpu-usage validation

--- src/main/java/at/gepardec/health/ReadinessCheck.java
+++ src/main/java/at/gepardec/health/ReadinessCheck.java
@@ -4,9 +4,11 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
 import org.jboss.logging.Logger;
+import com.sun.management.OperatingSystemMXBean;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.lang.management.ManagementFactory;
 
 @Readiness
 @ApplicationScoped
@@ -18,9 +20,13 @@ public class ReadinessCheck implements HealthCheck {
     @Inject
     State state;
 
+    OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+
     @Override
     public HealthCheckResponse call() {
         Log.info("Calling Readiness-Check for " + state.getServiceName());
+        Log.info("CPu-Usage: " + osBean.getProcessCpuLoad());
+        Log.info("CPu-Usage_System: " + osBean.getSystemLoadAverage());
         return HealthCheckResponse.named(state.getServiceName()).status(state.readiness).build();
     }
 }
