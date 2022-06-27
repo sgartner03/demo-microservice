249d64c4861f4488382376cdf6dc357a89b79023
Mario Oberwalder <mario.oberwalder@gepardec.com>
2022-06-27 16:17:03 +0200

    GPX-362 get uuid values from incoming headers

--- src/main/java/at/gepardec/service/MiddlemanService.java
+++ src/main/java/at/gepardec/service/MiddlemanService.java
@@ -1,5 +1,7 @@
 package at.gepardec.service;
 
+import at.gepardec.logging.UuidPropagationClientHeadersFactory;
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
@@ -11,6 +13,7 @@ import javax.ws.rs.core.Response;
 import java.util.UUID;
 
 @RegisterRestClient
+@RegisterClientHeaders(UuidPropagationClientHeadersFactory.class)
 public interface MiddlemanService {
 
     @GET
