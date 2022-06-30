4992d27f43af0f5ee19d6b17309510ae1e0b385f
Alexander Schober <alex.schober1@gmx.at>
2022-06-30 10:45:13 +0200

    changed application.properties

--- src/main/java/at/gepardec/health/State.java
+++ src/main/java/at/gepardec/health/State.java
@@ -1,8 +1,5 @@
 package at.gepardec.health;
 
-import org.jboss.logging.Logger;
-
-import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
@@ -10,23 +7,9 @@ public class State {
 
     private final String serviceName = "MainService";
 
-    @Inject
-    Logger Log;
-
     boolean liveness = true;
-    boolean readiness = true;
 
     public String getServiceName() {
         return serviceName;
-    }
-
-    public void toggleLiveness() {
-        Log.info("Toggling liveness");
-        liveness = !liveness;
-    }
-
-    public void toggleReadiness() {
-        Log.info("Toggling readiness");
-        readiness = !readiness;
     }
 }
